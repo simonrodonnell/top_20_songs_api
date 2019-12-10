75cecbdb92e5872e08623bcd196b214d09095d89
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-12-10 16:41:39 +0000

    added MusicContainer

---
+++ src/containers/MusicContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MusicContainer extends React.Component {
+
+render(){
+  return(
+    <h3>I'm a MusicContainer!</h3>
+  )
+}
+
+}
+
+export default MusicContainer
